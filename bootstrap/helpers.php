ee4991040e6527bdff59d84bffb2bb7ca55fccae
Nickel <a_shy_sky@163.com>
2018-10-31 06:51:36 +0000

    bugfix

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -8,7 +8,7 @@ function get_db_config()
         return $db_config = [
             'connection' => 'pgsql',
             'host'       => $url["host"],
-            'databse'    => substr($url['path'], 1),
+            'database'    => substr($url['path'], 1),
             'username'  => $url["user"],
             'password'  => $url["pass"],
         ];
